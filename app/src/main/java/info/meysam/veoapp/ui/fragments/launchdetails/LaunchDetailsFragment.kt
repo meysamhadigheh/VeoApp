38c3a81aad9191d6cb50246e1b77d33f2119cabb
Meysam Hadigheh <hadigheh.meysam@gmail.com>
2022-11-02 23:24:55 +0100

    add cores details to details screen

--- app/src/main/java/info/meysam/veoapp/ui/fragments/launchdetails/LaunchDetailsFragment.kt
+++ app/src/main/java/info/meysam/veoapp/ui/fragments/launchdetails/LaunchDetailsFragment.kt
@@ -6,23 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 import com.google.gson.Gson
 import info.meysam.veoapp.R
 import info.meysam.veoapp.data.model.Launch
-import info.meysam.veoapp.ui.customview.LaunchesList
-import info.meysam.veoapp.ui.fragments.launches.LaunchesLayout
+import info.meysam.veoapp.ui.theme.Shapes
+import info.meysam.veoapp.ui.theme.Typography
 import info.meysam.veoapp.ui.theme.VeoAppTheme
 
 class LaunchDetailsFragment : Fragment() {
@@ -31,29 +32,28 @@ class LaunchDetailsFragment : Fragment() {
     }
 
     private lateinit var viewModel: LaunchDetailsViewModel
-    private val args : LaunchDetailsFragmentArgs by navArgs()
+    private val args: LaunchDetailsFragmentArgs by navArgs()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(LaunchDetailsViewModel::class.java)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-
         return ComposeView(requireContext()).apply {
             setContent {
                 VeoAppTheme {
                     Surface(color = MaterialTheme.colors.background) {
-                        LaunchDetailsLayout(Gson().fromJson(
-                            args.launchData,
-                            Launch::class.java
-                        ))
+                        LaunchDetailsLayout(
+                            Gson().fromJson(
+                                args.launchData,
+                                Launch::class.java
+                            )
+                        )
                     }
                 }
             }
@@ -61,29 +61,125 @@ class LaunchDetailsFragment : Fragment() {
     }
 }
 
-@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun LaunchDetailsLayout(launch: Launch? = null) {
 
     launch?.let {
-        Card(
-            modifier = Modifier
-                .padding(horizontal = 15.dp, vertical = 12.dp)
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            elevation = 5.dp
-        ) {
-            Column(
-                modifier = Modifier.padding(all = 15.dp)
-            ) {
-                Text(it.name +  " | " +it.date_utc)
-//                Text(it.rocket)
-//                GlideImage(
-//                    model = it.links.patch.small,
-//                    contentDescription = null,
-//                    modifier = Modifier.size(72.dp),
-//                )
+        Column {
+            MyBasicCard(modifier = Modifier) {
+                TitleView(title = stringResource(R.string.cores_title))
+                launch.cores.forEach {
+                    Separator()
+                    TextWithDetail(title = stringResource(R.string.core), detail = it.core)
+                    TextWithDetail(title = stringResource(R.string.flight), detail = it.flight.toString())
+                    TextWithState(title = stringResource(R.string.gridfins), status = it.gridfins)
+                    TextWithState(title = stringResource(R.string.legs), status = it.legs)
+                    TextWithState(title = stringResource(R.string.reused), status = it.reused)
+                    TextWithState(title = stringResource(R.string.landing_attempt), status = it.landing_attempt)
+                    TextWithState(title = stringResource(R.string.landing_success), status = it.landing_success)
+                    TextWithDetail(title = stringResource(R.string.landing_type), detail = it.landing_type)
+                    TextWithDetail(title = stringResource(R.string.landpad), detail = it.landpad)
+                }
             }
         }
+    }
+}
+
+@Composable
+fun Separator() {
+    Box(
+        modifier = Modifier
+            .padding(start = 8.dp, end = 8.dp)
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(color = Color.LightGray)
+    )
+}
+
+@Composable
+fun MyBasicCard(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Card(modifier = modifier.padding(8.dp), shape = Shapes.medium, backgroundColor = Color.Black) {
+        Column(content = content)
+    }
+}
+
+@Composable
+fun TitleView(title: String?) {
+    if (title == null) return
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = title, style = Typography.h6, color = Color.White, modifier = Modifier
+                .align(
+                    Alignment.CenterHorizontally
+                )
+                .padding(10.dp)
+        )
+        Box(
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(color = Color.LightGray)
+        )
+    }
+}
+
+@Composable
+fun TextWithState(title: String?, status: Boolean?) {
+    if (title == null || status == null) return
+
+    Box(
+        contentAlignment = Alignment.Center, modifier = Modifier
+            .padding(6.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = title,
+            style = Typography.subtitle1,
+            color = Color.White,
+            modifier = Modifier.align(
+                Alignment.CenterStart
+            )
+        )
+        val painter =
+            if (status) painterResource(id = R.drawable.ic_baseline_check_circle_24) else painterResource(
+                id = R.drawable.ic_baseline_cancel_24
+            )
+        Image(
+            painter = painter, contentDescription = "status", modifier = Modifier.align(
+                Alignment.CenterEnd
+            )
+        )
+    }
+}
+
+@Composable
+fun TextWithDetail(title: String?, detail: String?) {
+    if (title == null || detail == null) return
+    Box(
+        contentAlignment = Alignment.Center, modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = title,
+            style = Typography.subtitle1,
+            color = Color.White,
+            modifier = Modifier.align(
+                Alignment.CenterStart
+            )
+        )
+        Text(
+            text = detail,
+            style = Typography.caption,
+            color = Color.White,
+            modifier = Modifier.align(
+                Alignment.CenterEnd
+            )
+        )
     }
 }
